9ae980d043f366577833e7d177ea63809a80b126
MGovier <merlingovier@gmail.com>
2015-11-04 18:04:45 +0000

    Added draft schema and autoform.

--- client/lib/client.js
+++ client/lib/client.js
@@ -6,6 +6,43 @@ Meteor.startup(() => {
 // counter starts at 0
 Session.setDefault('counter', 0);
 
+Schemas = {};
+
+Template.registerHelper("Schemas", Schemas);
+
+Schemas.Scheme = new SimpleSchema({
+	rubric: {
+		type: [Object],
+		minCount: 1
+	},
+	'rubric.$.mark': {
+		type: Number
+	},
+	'rubric.$.criteria': {
+		type: String,
+		optional: true
+	},
+	comments: {
+		type: [String],
+		optional: true
+	},
+	allowAdjustment:{
+		type: Boolean
+	}
+})
+
+Template.body.events({
+	'click table tr': function(){
+
+	}
+});
+
+Template.body.onRendered(function() {
+	$('.ui.menu .ui.dropdown').dropdown({
+        on: 'hover'
+      });
+});
+
 Template.hello.helpers({
   counter: function () {
     return Session.get('counter');
