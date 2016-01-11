4876a7f66f23f90799444a43c07e618bff86ae54
MGovier <merlingovier@gmail.com>
2016-01-11 17:39:01 +0000

    Use appcache, and control scheme publications by user or unique url ID.

--- server/main.js
+++ server/main.js
@@ -2,6 +2,12 @@ Meteor.startup(function () {
     // code to run on server at startup
  });
 
-Meteor.publish("markingSchemes", function () {
-	return MarkingSchemes.find();
+Meteor.publish("markingSchemes", function (idArg) {
+  // Return nothing if not logged in.
+  if (idArg) {
+    return MarkingSchemes.find({_id: idArg});
+  } else {
+    if (!this.userId) return [];
+    return MarkingSchemes.find({creator: this.userId});
+  }
 });
