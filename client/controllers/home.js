1ce3b703e7666a21212da18811bd7646d8b66852
MGovier <merlingovier@gmail.com>
2016-03-22 17:23:34 +0000

    Remove unused trackers.

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,33 +1,16 @@
-Template.home.onCreated(function() {
-  let intervalA = Meteor.setInterval(function() {
-    $('.right-perspective-overlay').visibility('refresh');
-  }, 20);
-  let intervalB = Meteor.setInterval(function() {
-    $('.left-perspective-overlay').visibility('refresh');
-  }, 20);
-  Session.set('intervalTracker', [intervalA, intervalB]);
-});
-
 Template.home.onRendered(() => {
   $('.right-perspective-overlay').transition('slide right in');
   $('.left-perspective-overlay').transition('slide left in');
 });
 
-Template.home.onDestroyed(() => {
-  let intervals = Session.get('intervalTracker');
-  intervals.forEach((id) => {
-    Meteor.clearInterval(id);
-  });
-});
-
 Template.home.events({
-  'click .google-log-in': function() {
+  'click .google-log-in'() {
     Meteor.loginWithGoogle();
   },
-  'click .create-scheme': function() {
+  'click .create-scheme'() {
     Router.go('insertScheme');
   },
-  'click .view-schemes': function() {
+  'click .view-schemes'() {
     Router.go('dashboard');
-  }
+  },
 });
