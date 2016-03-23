7c1fc0b141afda431f16a2c91cec5ef6324b1147
MGovier <merlingovier@gmail.com>
2016-03-23 14:38:48 +0000

    Don't wait for subs when offline.
    Fix #43

---
+++ client/lib/globalHelpers.js
@@ -0,0 +1,10 @@
+/**
+ * Helper functions available to all templates.
+ */
+
+/**
+ * Only display loading when we have server connection.
+ * Otherwise we use local data.
+ */
+Template.registerHelper('connectionReady', () =>
+  Template.instance().subscriptionsReady || !Meteor.status().connected);
