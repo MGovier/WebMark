0dfdcdd8a730a4bed97218fe3d3bdcb11de1ab1e
MGovier <merlingovier@gmail.com>
2016-03-26 02:28:10 +0000

    Forward users automatically after log-in.

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,6 +1,11 @@
-Template.home.onRendered(() => {
+Template.home.onRendered(function render() {
   $('.right-perspective-overlay').transition('slide right in');
   $('.left-perspective-overlay').transition('slide left in');
+  this.autorun(() => {
+    if (Meteor.userId() && FlowRouter.current().path === '/') {
+      FlowRouter.go('dashboard');
+    }
+  });
 });
 
 Template.home.events({
